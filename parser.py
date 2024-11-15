d66c0bfac1b1603a17656778b2017685dff3efc7
Deaponn <bartek.sajo@interia.pl>
2024-11-15 18:08:35 +0100

    Refactor grammar to a simpler declaration

--- parser.py
+++ parser.py
@@ -2,8 +2,6 @@ from sly import Parser as SlyParser
 from scanner import Scanner
 from AST import *
 
-# TODO: change if "..." in p._namemap to something better
-
 class Parser(SlyParser):
 
     tokens = Scanner.tokens
@@ -22,70 +20,82 @@ class Parser(SlyParser):
     def __init__(self):
         self.names = { }
 
-    @_('block start', 'block')
+    @_('block')
     def start(self, p):
-        if "start" in p._namemap:
-            return StartNode(p.block, p.start)
-        else:
-            return StartNode(p.block)
+        return StartNode(p.block)
 
-    @_('statement', '"{" next_statements "}"')
+    @_('block start')
+    def start(self, p):
+        return StartNode(p.block, p.start)
+
+    @_('statement')
     def block(self, p):
-        if "statement" in p._namemap:
-            return Statement(p.statement)
-        else:
-            return p.next_statements
+        return Statement(p.statement)
 
-    @_('statement next_statements', 'statement')
+    @_('"{" next_statements "}"')
+    def block(self, p):
+        return p.next_statements
+
+    @_('statement')
     def next_statements(self, p):
-        if "next_statements" in p._namemap:
-            return Statement(p.statement, p.next_statements)
-        else:
-            return Statement(p.statement)
+        return Statement(p.statement)
+
+    @_('statement next_statements')
+    def next_statements(self, p):
+        return Statement(p.statement, p.next_statements)
 
     @_('action_statement ";"', 'flow_control_statement')
     def statement(self, p):
         return p[0]
 
-    @_('id_expr "=" expr', 'id_expr PASSIGN expr', 'id_expr MASSIGN expr', 'id_expr TASSIGN expr',
-        'id_expr DASSIGN expr', 'PRINT values', 'RETURN expr', 'CONTINUE', 'BREAK')
+    @_('id_expr "=" expr', 'id_expr PASSIGN expr', 'id_expr MASSIGN expr',
+        'id_expr TASSIGN expr', 'id_expr DASSIGN expr')
     def action_statement(self, p):
-        if len(p) == 3: # must handle "left hand of assign is not an ID" error
-            return AssignStatement(p.id_expr, p[1], p.expr)
-        elif "RETURN" in p._namemap:
-            return ReturnValue(p.expr)
-        elif "PRINT" in p._namemap:
-            return PrintValue(p.values)
-        else:
-            return LoopControlNode(p[0]) # catch both CONTINUE and BREAK
-        # print("def statement, id = expr")
-        # self.names[p.ID] = p.expr
+        return AssignStatement(p.id_expr, p[1], p.expr)
 
-    @_('INT "," indexes', 'INT')
+    @_('PRINT values')
+    def action_statement(self, p):
+        return PrintValue(p.values)
+
+    @_('RETURN expr')
+    def action_statement(self, p):
+        return ReturnValue(p.expr)
+
+    @_('CONTINUE', 'BREAK')
+    def action_statement(self, p):
+        return LoopControlNode(p[0])
+
+    @_('INT')
     def indexes(self, p):
-        if "indexes" in p._namemap:
-            return IndexList(p.INT, p.indexes)
-        else:
-            return IndexList(p.INT)
+        return IndexList(p.INT)
 
-    @_('expr "," values', 'expr')
+    @_('INT "," indexes')
+    def indexes(self, p):
+        return IndexList(p.INT, p.indexes)
+
+    @_('expr')
     def values(self, p):
-        if "values" in p._namemap:
-            return ValueList(p.expr, p.values)
-        else:
-            return ValueList(p.expr)
+        return ValueList(p.expr)
 
-    @_('IF expr block %prec IFX', 'IF expr block ELSE block',
-        'WHILE expr block', 'FOR ID "=" range block')
+    @_('expr "," values')
+    def values(self, p):
+        return ValueList(p.expr, p.values)
+
+    @_('IF expr block %prec IFX')
     def flow_control_statement(self, p):
-        if "IF" in p._namemap and "ELSE" in p._namemap:
-            return IfStatement(p.expr, p.block0, p.block1)
-        elif "IF" in p._namemap:
-            return IfStatement(p.expr, p.block)
-        elif "WHILE" in p._namemap:
-            return WhileStatement(p.expr, p.block)
-        elif "FOR" in p._namemap:
-            return ForStatement(p.ID, p.range, p.block)
+        return IfStatement(p.expr, p.block)
+
+    @_('IF expr block ELSE block')
+    def flow_control_statement(self, p):
+        return IfStatement(p.expr, p.block0, p.block1)
+
+    @_('WHILE expr block')
+    def flow_control_statement(self, p):
+        return WhileStatement(p.expr, p.block)
+
+    @_('FOR ID "=" range block')
+    def flow_control_statement(self, p):
+        return ForStatement(p.ID, p.range, p.block)
 
     @_('expr "+" expr', 'expr "-" expr',
         'expr "*" expr', 'expr "/" expr',
@@ -129,36 +139,38 @@ class Parser(SlyParser):
     def range(self, p):
         return RangeNode(p.expr0, p.expr1)
 
-    @_('INT', 'FLOAT', 'STRING', 'id_expr')
+    @_('INT', 'FLOAT', 'STRING')
     def expr(self, p):
-        if "id_expr" in p._namemap:
-            return p.id_expr
-        else:
-            return ValueNode(p[0])
-        # return p[0]
+        return ValueNode(p[0])
+
+    @_('id_expr')
+    def expr(self, p):
+        return p.id_expr
         # try:
         #     return self.names[p.ID]
         # except LookupError:
         #     print("Undefined ID '%s'" % p.ID)
         #     return 0
 
-    @_('ID', 'ID "[" indexes "]"')
+    @_('ID')
     def id_expr(self, p):
-        if "indexes" in p._namemap:
-            return IndexedVariable(p.ID, p.indexes)
-        else:
-            return Variable(p.ID)
+        return Variable(p.ID)
+
+    @_('ID "[" indexes "]"')
+    def id_expr(self, p):
+        return IndexedVariable(p.ID, p.indexes)
 
     @_('"[" outerlist "]"')
     def expr(self, p):
         return Outerlist(p.outerlist)
 
-    @_('"[" values "]" "," outerlist', '"[" values "]"')
+    @_('"[" values "]"')
     def outerlist(self, p):
-        if "outerlist" in p._namemap:
-            return Outerlist(p.values, p.outerlist)
-        else:
-            return Outerlist(p.values)
+        return Outerlist(p.values)
+
+    @_('"[" values "]" "," outerlist')
+    def outerlist(self, p):
+        return Outerlist(p.values, p.outerlist)
 
     @_('ZEROS "(" expr ")"')
     def expr(self, p):
